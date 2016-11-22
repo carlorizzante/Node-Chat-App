750f17e55667a790fbb8c8b9c62684156174b5f9
Carlo Rizzante <hello@carlorizzante.com>
2016-11-22 10:54:02 +0100

    Adds basic Socket.io server and client side

--- server/server.js
+++ server/server.js
@@ -1,23 +1,35 @@
-const express = require("express");
+const http = require("http");
 const path = require("path");
 const hbs = require("hbs");
+const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.set("view engine", "hbs");
 
-app.get("/", (req, res) => {
-  res.status(200).render("index.html");
-  // res.status(200).render("index.hbs", {
-  //   msg: "Server here, saying hi!"
-  // });
+io.on("connection", socket => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected.");
+  });
 });
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+  // res.status(200).render("index.html");
+  res.status(200).render("index.hbs", {
+    msg: "Server here, saying hi!"
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
