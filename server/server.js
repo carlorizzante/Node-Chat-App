aca4aa56bd1f1ac231efc5c662f2f7f4b5d9ea02
Carlo Rizzante <hello@carlorizzante.com>
2016-11-22 09:23:02 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const publicPath = path.join(__dirname, "../public");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  // res.status(200).render("index.html");
+  res.status(200).render("index.hbs", {
+    msg: "Server here, saying hi!"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
